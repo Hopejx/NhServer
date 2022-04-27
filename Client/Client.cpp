4f2f57440da045413a651641d46bc489ee10afc4
Hope <neu_jx@163.com>
2022-04-27 20:24:23 +0800

    Client升级

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -2,9 +2,36 @@
 #define WIN32_LEAN_AND_MEAN 
 #include<Windows.h>
 #include<WinSock2.h>
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+//#define _WINSOCK_DEPRECATED_NO_WARNINGS
 using namespace std;
+#pragma warning(disable:4996)
 
+enum CMD {
+	CMD_LOGIN,
+	CMD_LOGOUT,
+	CMD_ERROR
+};
+//消息头
+struct DataHeader {
+	short dataLength;// 数据长度
+	short cmd;//命令
+};
+//登入
+struct LogIn {
+	char userName[32];
+	char PassWord[32];
+};
+struct LogInResule {
+	int result;
+};
+//登出
+struct LogOut {
+	char userName[32];
+};
+
+struct LogOutResule {
+	int result;
+};
 
 int main()
 {
@@ -15,10 +42,10 @@ int main()
 	//1. 建立一个Socket
 	SOCKET _socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (INVALID_SOCKET == _socket) {
-		cout << "错误，建立失败" << endl;
+		cout << "错误，建立Socket失败" << endl;
 	}
 	else {
-		cout << "成功，建立成功" << endl;
+		cout << "成功，建立Socket成功" << endl;
 	}
 
 	//2. 连接服务器 connect
@@ -27,22 +54,58 @@ int main()
 	_sin.sin_port = htons(4567);
 	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	if (INVALID_SOCKET == connect(_socket, (sockaddr*)(&_sin), sizeof _sin)) {
-		cout << "连接失败" << endl;
+		cout << "连接服务器失败" << endl;
 	}
 	else {
-		cout << "连接成功" << endl;
+		cout << "连接服务器成功" << endl;
 	}
-	//3. 接受服务器信息
-	char rebuff[256] = {};
-	//调用recv
-	int nlen = recv(_socket, rebuff, 256, 0);
-	if (nlen > 0) {
-		cout << "接受到数据" << endl;
-		printf("%s\n", rebuff);
+
+	char cmdBuff[128] = {};
+	while (true) {
+		//3. 输入请求命令
+		scanf("%s", &cmdBuff);
+		//4. 处理请求命令
+		if (0 == strcmp(cmdBuff, "exit")) {
+			cout << "循环结束" << endl;
+			break;
+		}
+		else if (0 == strcmp(cmdBuff, "login")) {
+			LogIn Login = { "hope","123456" };
+			DataHeader dh = { sizeof(Login) ,CMD_LOGIN };
+			
+			//先发数据头
+			send(_socket, (const char *)&dh, sizeof(dh), 0);
+			send(_socket, (const char *)&Login, sizeof (Login), 0);
+			//接受服务器返回的数据
+			DataHeader retHeaderin = {};
+			LogInResule Loginresult = {};
+			recv(_socket, (char*)&retHeaderin, sizeof retHeaderin, 0);
+			recv(_socket, (char*)&Loginresult, sizeof Loginresult, 0);
+			cout << "LoginResult:" << Loginresult.result << endl;
+		}
+		else if (0 == strcmp(cmdBuff, "logout")) {
+			LogOut logout = { "hope" };
+			DataHeader dh = {sizeof logout ,CMD_LOGOUT };
+			//向服务器发送
+			send(_socket, (char *)&dh, sizeof(dh), 0);
+			send(_socket, (char *)&logout, sizeof (logout), 0);
+			//接收
+			DataHeader retHeaderout = {};
+			LogOutResule Logoutresult = {};
+			recv(_socket, (char *)&retHeaderout, sizeof retHeaderout, 0);
+			recv(_socket, (char *)&Logoutresult, sizeof Logoutresult, 0);
+			cout << "LoginResult:" << Logoutresult.result << endl;
+		}
+		else {
+			cout << "不支持的命令：重新输入" << endl;
+		}
 	}
-	//4. 关闭套接字
+
+	//7. 关闭套接字
 	closesocket(_socket);
 	WSACleanup();
+	cout << "已退出，任务结束" << endl;
+	getchar();
 	return 0;
 }
 
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #define WIN32_LEAN_AND_MEAN 
 #include<Windows.h>
+#include<string>
 #include<WinSock2.h>
 using namespace std;
-
+#pragma warning(disable:4996)
 /* 只能在win条件下
 //调用 win 静态链接库
 #pragma comment(lib,"ws2_32.lib")
 */
+enum CMD {
+	CMD_LOGIN,
+	CMD_LOGOUT,
+	CMD_ERROR
+};
 
+//消息头
+struct DataHeader {
+	short dataLength;// 数据长度
+	short cmd;//命令
+};
+//登入
+struct LogIn {
+	char userName[32];
+	char PassWord[32];
+};
+struct LogInResule {
+	int result;
+};
+//登出
+struct LogOut {
+	char userName[32];
+};
+
+struct LogOutResule {
+	int result;
+};
 
 int main()
 {
@@ -19,58 +46,103 @@ int main()
 	//1. 建立一个Socket
 	//ipv4  字节流 tcp类型
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 	//2. bind 绑定用于接受客户端连接的网路端口
 	// 创建一个sockaddr 结构体 存放socket信息
 	sockaddr_in _sin = {};
+
 	_sin.sin_family = AF_INET;//指定ipv4协议 ipv6 AF_INET6
 	_sin.sin_port = htons(4567); // host to net unsigned short
+
 	//sin_addr  ... 是一个联合体 不是一个对象套对象
 	//INADDR_ANY 不限定ip地址 任何一个都可以
 	_sin.sin_addr.S_un.S_addr = INADDR_ANY;// inet_addr("127.0.0.1");
 
 	if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof _sin)) {
-		cout << "绑定失败" << endl;
+		cout << "绑定端口失败" << endl;
 	}
 	else {
-		cout << "绑定成功" << endl;
+		cout << "绑定端口成功" << endl;
 	}
 
 	//3. listen 侦听端口
 	//最大同时5个连接
 	if (SOCKET_ERROR == listen(_sock, 5)) {
-		cout << "监听失败" << endl;
+		cout << "监听端口失败" << endl;
 	}
 	else {
-		cout << "监听成功" << endl;
+		cout << "监听端口成功" << endl;
 	}
-	//4 accept 等待接受客户端连接
 
+	//4 accept 等待接受客户端连接
 	//这个socket接受的是客户端的信息
 	sockaddr_in _clientAddr = { };
+	int nAddrLen = sizeof _clientAddr;
 	//int nAddrLen = sizeof sockaddr_in;
 	//accept(_sock, (sockaddr*)&_clientAddr, &nAddrLen);
 	//接受客户端的信息 socket 存到这个socket里面
 	SOCKET _clientSocket = INVALID_SOCKET;
-
 	char buff[] = "hello client,I am Server";
 
+	_clientSocket = accept(_sock, (sockaddr*)&_clientAddr, &nAddrLen);
+	if (INVALID_SOCKET == _clientSocket) {
+		cout << "接收到无效客户端socket" << endl;
+	}
+	cout << "新客户端加入" << "IP为：" << inet_ntoa(_clientAddr.sin_addr) << endl;
+	char Rebuff[128] = {};
+
 	while (true) {
-		_clientSocket = accept(_sock, (sockaddr*)&_clientAddr, (int*)(sizeof _clientAddr));
-		if (INVALID_SOCKET == _clientSocket) {
-			cout << "接收到无效客户端socket" << endl;
+		DataHeader header = {};
+
+		// 5. 接收客户端发送的请求
+		int nLen = recv(_clientSocket, (char *)&header, sizeof DataHeader, 0);
+		
+		if (nLen <= 0) {
+			cout << "客户端已退出,任务结束" << endl;
+			break;
 		}
-		else {
-			//cout << "接收到有效客户端socket" << endl;
+		cout << "收到命令：" << header.cmd << "收到数据长度：" << header.dataLength<<endl;
+		
+		
+		//6. 处理请求
+		switch (header.cmd) {
+			case CMD_LOGIN: {
+				LogIn login = {};
+				recv(_clientSocket, (char *)&login, sizeof LogIn, 0);
+				//忽略判断用户密码是否正确的过程
+				LogInResule ret = { 0 };
+
+				send(_clientSocket, (char *)&header, sizeof DataHeader, 0);
+				send(_clientSocket, (char *)&ret, sizeof LogInResule, 0);
+			
+			}
+			break;
+			case CMD_LOGOUT: {
+				LogOut logout = {};
+				recv(_clientSocket, (char *)&logout, sizeof LogOut, 0);
+				//忽略判断用户密码是否正确的过程
+				LogOutResule ret = { 0 };
+
+				send(_clientSocket, (char *)&header, sizeof DataHeader, 0);
+				send(_clientSocket, (char *)&ret, sizeof LogOutResule, 0);
+			}
+			break;
+			default: 
+				header.cmd = CMD_ERROR;
+				header.dataLength = 0;
+				send(_clientSocket, (char *)&header, sizeof DataHeader, 0);
+			
+			break;
 		}
 
-		//cout << "新客户端加入" << "IP为：" << inet_ntoa(_clientAddr.sin_addr) << endl;
-		//5. send 向客户端发送一条数据
-		send(_clientSocket, buff, strlen(buff) + 1, 0);
 	}
-	//6. 关闭套接字
+
+	//8. 关闭套接字
 	closesocket(_sock);
 
 	WSACleanup();
+	cout << "已退出，任务结束" << endl;
+	getchar();
 	return 0;
 }
 
